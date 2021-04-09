3e34410b053ac71d6e9441d37bc4f65a2cc5c97d
Fermin <pureco.fermin1@gmail.com>
2021-04-09 16:06:31 -0700

    Updating config file
    Adding more info to config file.

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // provide Database information.
     $config = [
-        'DB_DATABASE' => 'edit this',
-        'DB_USERNAME' => 'edit this',
-        'DB_PASSWORD' => 'edit this'
+        'DB_DATABASE' => '',
+        'DB_USERNAME' => '',
+        'DB_PASSWORD' => ''
     ];
